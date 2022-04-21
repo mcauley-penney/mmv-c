fa3c19b7cb2fb556cc4d1be8e8cf10c5a32e46dc
McAuley Penney <jacobmpenney@gmail.com>
2022-04-21 06:30:26 -0700

    feat, fix:
        • feat:
            • read file lines into a char * array: read_strarr_from_file()
                • known issue: will fail if user enters amount of
                  newlines inside of the range of lines that are within the
                  amount of argc
                    • for example, if the program is expecting to collect
                      two arguments from a buffer, entering some amount of
                      newlines within the first two lines will crash the
                      program.
    
            • rename files in filesystem: rename_files()
    
        • fix: write_strarr_to_file()
            • correct array index used from the total count of args to
              working index

--- src/mmv.c
+++ src/mmv.c
@@ -1,3 +1,8 @@
+/**
+ * TODO:
+ * 1. produce documentation
+ */
+
 #include "mmv.h"
 
 int main(int argc, char *argv[])
@@ -8,15 +13,37 @@ int main(int argc, char *argv[])
     // remove program invocation from argv and decrement argc
     rm_strarr_index(argv, &argc, 0);
 
+    char *new_name_arr[argc];
+
+    // TODO: remove duplicate inputs
+    //  - start with str comparison
+    //  - advance to some higher form, like hashing
+
     tmp_path = mk_uniq_path("/tmp/mmv_", ".txt", &tmp_path_len);
 
-    // open file for writing
     write_strarr_to_file(tmp_path, argv, argc);
 
-    // open in buffer
     open_file_in_buf(tmp_path, tmp_path_len);
 
+    // TODO: digest newlines in temp buffer
+    read_strarr_from_file(tmp_path, tmp_path_len, new_name_arr);
+
+    /**
+     * TODO: add protections
+     *  - disallow duplicate renames
+     *  - allow swapping and cycling names
+     *  - make sure we can handle some common escape chars in names
+     *      - \n
+     *
+     *  - Idea: rename all items to temp names then proceed with rename to
+     *          chosen new name
+     *          - can this be done on a per-filename basis?
+     */
+
+    rename_files(argv, new_name_arr, argc);
+
     destroy_path(tmp_path);
+    free_strarr(new_name_arr, argc);
 
     return EXIT_SUCCESS;
 }
@@ -28,9 +55,17 @@ void destroy_path(char *path)
     rm_success = remove(path);
 
     if (rm_success == -1)
-        printf("ERROR: Unable to delete \"%s\"", path);
+        printf("ERROR: Unable to delete \"%s\"\n", path);
 
     free(path);
+}
+
+void free_strarr(char *strarr[], const int arg_count)
+{
+    int i;
+
+    for (i = 0; i < arg_count; i++)
+        free(strarr[i]);
 }
 
 void open_file(char *path, char *mode, FILE **fptr)
@@ -80,6 +115,60 @@ char *mk_uniq_path(const char *prefix, const char *ext, int *path_len)
     return path;
 }
 
+void read_strarr_from_file(char *path, const int arg_count, char *strarr[])
+{
+    const int max_str_len = 500;
+    char *cur_str = malloc(max_str_len * sizeof(cur_str)), *read_ptr;
+    FILE *fptr = NULL;
+    int i, j = 0;
+
+    open_file(path, "r", &fptr);
+
+    for (i = 0; i < arg_count && read_ptr != NULL; i++)
+    {
+        read_ptr = fgets(cur_str, max_str_len, fptr);
+
+        if (read_ptr != NULL)
+        {
+            strarr[j] = malloc(max_str_len * sizeof(strarr[j]));
+            rm_str_nl(cur_str);
+            strcpy(strarr[j], cur_str);
+            j++;
+        }
+    }
+
+    fclose(fptr);
+    free(cur_str);
+}
+
+// TODO:
+void rename_files(char *old_nm_arr[], char *new_nm_arr[], const int arg_count)
+{
+    int i, rename_result;
+
+    for (i = 0; i < arg_count; i++)
+    {
+        char *old_nm = old_nm_arr[i];
+        char *new_nm = new_nm_arr[i];
+
+        rename_result = rename(old_nm, new_nm);
+
+        if (rename_result == -1)
+            printf("ERROR: Could not rename \"%s\" to \"%s\"", old_nm, new_nm);
+    }
+}
+
+// https://stackoverflow.com/a/42564670
+void rm_str_nl(char *str)
+{
+    char *end = str + strlen(str) - 1;
+
+    while (end > str && isspace(*end))
+        end--;
+
+    *(end + 1) = '\0';
+}
+
 void rm_strarr_index(char *strarr[], int *arr_len, int index)
 {
     int i;
@@ -98,7 +187,7 @@ void write_strarr_to_file(char *path, char *args[], const int arg_count)
     open_file(path, "w", &fptr);
 
     for (i = 0; i < arg_count; i++)
-        fprintf(fptr, "%s", args[arg_count]);
+        fprintf(fptr, "%s\n", args[i]);
 
     fclose(fptr);
 }
--- src/mmv.h
+++ src/mmv.h
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,8 +6,12 @@
 #include <time.h>
 
 void destroy_path(char *path);
+void free_strarr(char *strarr[], const int arg_count);
 char *mk_uniq_path(const char *prefix, const char *ext, int *path_len);
 void open_file(char *path, char *mode, FILE **fptr);
 void open_file_in_buf(const char *path, const int path_len);
+void read_strarr_from_file(char *path, const int arg_count, char *strarr[]);
+void rename_files(char *old_nm_arr[], char *new_nm_arr[], const int arg_count);
+void rm_str_nl(char *str);
 void rm_strarr_index(char *strarr[], int *arr_len, int index);
 void write_strarr_to_file(char *path, char *args[], const int arg_count);
