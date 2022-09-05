e1705837a905f2c7bcd7d1f0ca8a776659d7424d
McAuley Penney <jacobmpenney@gmail.com>
2022-09-04 18:18:40 -0700

    test: add test for file writing
    - some simple renames, eg map → set

--- test/test_main.c
+++ test/test_main.c
@@ -1,8 +1,17 @@
 #include "test_mmv.h"
+#include <sys/stat.h>
 
 int main(void)
 {
-    assert_hashmap_correct_creation();
+    printf("--BEGINNING UNIT TESTING--\n");
+
+    printf("Testing set creation...\n");
+    verify_correct_set_creation();
+    printf("Done!\n");
+
+    printf("Testing file output capabilities...\n");
+    verify_correct_write_to_file();
+    printf("Done!\n");
 
     return EXIT_SUCCESS;
 }
--- test/test_mmv.c
+++ test/test_mmv.c
@@ -1,35 +1,110 @@
 #include "test_mmv.h"
 #include "../src/mmv.h"
+#include <math.h>
 
-void assert_hashmap_correct_creation(void)
+void verify_correct_set_creation(void)
 {
-    int argc = 11;
-    char *argv_11[11] = {"TEST_INVOCATION", "test1.txt", "test2.txt", "test3.txt", "test4.txt", "test5.txt",
-                         "test6.txt",       "test7.txt", "test8.txt", "test9.txt", "test10.txt"};
+    /**
+     * Cases to test:
+     *  1. number of inputs < 2
+     *      • returns NULL
+     *  2. No duplicate inputs
+     *      • Resulting set has
+     *          • (argc - 1) args
+     *          • argv, without invocation, in set, in order
+     *  3. Some duplicate inputs
+     *      • Resulting set has
+     *          • (argc - num_duplicates) args
+     *          • argv, without invocation and duplicates, in set, in order
+     **/
 
-    assert_hashmap_size_and_content(argc, argv_11);
+    int argc;
+    size_t correct_argc;
 
     argc = 1;
-    char *argv[1] = {"TEST_INVOCATION"};
+    char *null_argv[1] = {"INVOCATION"};
 
+    assert_set_null(argc, null_argv);
+
+    argc = 11;
+    correct_argc = 10;
+    char *argv11[11] = {"INVOCATION", "a.out", "b.out", "c.out", "d.out", "e.out",
+                        "f.out",      "g.out", "h.out", "i.out", "j.out"};
+    char *correct_argv11[10] = {"a.out", "b.out", "c.out", "d.out", "e.out",
+                                "f.out", "g.out", "h.out", "i.out", "j.out"};
+
+    assert_set_size_and_content(argc, correct_argc, argv11, correct_argv11);
+
+    argc = 11;
+    correct_argc = 3;
+    char *argv_duplicates[11] = {"INVOCATION", "test1.txt", "test1.txt", "test2.txt", "test2.txt", "test3.txt",
+                                 "test1.txt",  "test2.txt", "test3.txt", "test1.txt", "test1.txt"};
+
+    char *correct_argv_duplicates[3] = {"test1.txt", "test2.txt", "test3.txt"};
+
+    assert_set_size_and_content(argc, correct_argc, argv_duplicates, correct_argv_duplicates);
+}
+
+void assert_set_null(int argc, char *argv[])
+{
     struct Set *test_set = make_str_set(argc, argv);
 
     assert(test_set == NULL);
 }
 
-void assert_hashmap_size_and_content(int argc, char *argv[])
+void assert_set_size_and_content(int argc, size_t correct_argc, char *argv[], char *correct_argv[])
 {
     char *cur_str;
-    size_t expected_argc = (size_t)argc - 1, i;
+    size_t cur_key, i;
 
     struct Set *test_set = make_str_set(argc, argv);
 
-    assert(expected_argc == test_set->num_keys);
+    assert(correct_argc == test_set->num_keys);
 
     // iterate over argv and each string at i must match
-    for (i = 0; i < expected_argc; i++)
+    for (i = 0; i < correct_argc; i++)
     {
-        cur_str = test_set->map[test_set->keys[i]];
-        assert(strcmp(argv[i + 1], cur_str) == 0);
+        cur_key = test_set->keys[i];
+        cur_str = test_set->map[cur_key];
+        assert(strcmp(correct_argv[i], cur_str) == 0);
     }
+
+    free_str_set(test_set);
+}
+
+void verify_correct_write_to_file(void)
+{
+    int argc = 11;
+    char *argv[11] = {"INVOCATION", "a.out", "b.out", "c.out", "d.out", "e.out",
+                      "f.out",      "g.out", "h.out", "i.out", "j.out"};
+    char tmp_path[] = "/tmp/mmv_XXXXXX";
+    char cur_str[PATH_MAX], *read_ptr = "";
+    size_t i = 0;
+
+    struct Set *test_set = make_str_set(argc, argv);
+
+    umask(077);
+
+    assert(write_strarr_to_tmpfile(test_set, tmp_path) == 0);
+
+    // Lifted out of reading function and modified
+    FILE *tmp_fptr = fopen(tmp_path, "r");
+    assert(tmp_fptr != NULL);
+
+    while (read_ptr != NULL && i < test_set->num_keys)
+    {
+        read_ptr = fgets(cur_str, PATH_MAX, tmp_fptr);
+
+        if (read_ptr != NULL && strcmp(cur_str, "\n") != 0)
+        {
+            cur_str[strlen(cur_str) - 1] = '\0';
+            assert(strcmp(test_set->map[test_set->keys[i]], cur_str) == 0);
+            i++;
+        }
+    }
+
+    fclose(tmp_fptr);
+
+    rm_path(tmp_path);
+    free_str_set(test_set);
 }
