40a49623a59629849b50a9c89a8f944b56469208
McAuley Penney <jacobmpenney@gmail.com>
2023-01-09 18:37:27 -0700

    feat: add commandline flag parsing
    - add help, version, verbose, and a default
      error message for unsupported flags
    
    - update some prototype docs in mmv.h

--- main.c
+++ main.c
@@ -2,30 +2,97 @@
 
 int main(int argc, char *argv[])
 {
-    struct Set *mmv_set = make_str_set(argc, argv);
-    if (mmv_set == NULL)
-        exit(EXIT_FAILURE);
+	int flag_arg;
+	char short_opts[]				 = "hvV";
+	int option_index				 = 0;
+	static struct option long_opts[] = {
+		{"help", no_argument, NULL, 'h'},
+		{"verbose", no_argument, NULL, 'v'},
+		{"version", no_argument, NULL, 'V'},
+	};
+	struct Opts *options = make_opts();
 
-    char tmp_path[] = "/tmp/mmv_XXXXXX";
+	while ((flag_arg = getopt_long(
+				argc, argv, short_opts, long_opts, &option_index)) != -1)
+	{
+		switch (flag_arg)
+		{
+		case 'h':
+			free(options);
+			usage();
+			return EXIT_SUCCESS;
 
-    umask(077);
+		case 'v':
+			options->verbose = true;
+			break;
 
-    if (write_strarr_to_tmpfile(mmv_set, tmp_path) != 0)
-        goto rm_path_out;
+		case 'V':
+			free(options);
+			printf("mmv version %s\n", PROG_VERSION);
+			return EXIT_SUCCESS;
 
-    if (open_file_in_editor(tmp_path) != 0)
-        goto rm_path_out;
+		default:
+			free(options);
+			try_help();
+			return EXIT_FAILURE;
+		}
+	}
 
-    if (rename_filesystem_items(mmv_set, tmp_path) != 0)
-        goto rm_path_out;
+	argv += optind;
+	argc -= optind;
 
-    rm_path(tmp_path);
-    free_str_set(mmv_set);
+	struct Set *mmv_set = make_str_set(argc, argv);
+	if (mmv_set == NULL)
+	{
+		free(options);
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	char tmp_path[] = "/tmp/mmv_XXXXXX";
+
+	umask(077);
+
+	if (write_strarr_to_tmpfile(mmv_set, tmp_path) != 0)
+		goto rm_path_out;
+
+	if (open_file_in_editor(tmp_path) != 0)
+		goto rm_path_out;
+
+	if (rename_filesystem_items(options, mmv_set, tmp_path) != 0)
+		goto rm_path_out;
+
+	rm_path(tmp_path);
+	free(options);
+	free_str_set(mmv_set);
+
+	return EXIT_SUCCESS;
 
 rm_path_out:
-    rm_path(tmp_path);
-    free_str_set(mmv_set);
-    return EXIT_FAILURE;
+	rm_path(tmp_path);
+	free(options);
+	free_str_set(mmv_set);
+	return EXIT_FAILURE;
+}
+
+struct Opts *make_opts()
+{
+	struct Opts *opts = malloc(sizeof(struct Opts));
+	if (opts == NULL)
+		return NULL;
+
+	opts->verbose = false;
+
+	return opts;
+}
+
+void try_help()
+{
+	puts("Try 'mmv [-h][--help]'for more information");
+}
+
+void usage()
+{
+	printf("Usage: %s [OPTION] SOURCES\n\n", PROG_NAME);
+	puts("Rename or move SOURCE(s) by editing them in $EDITOR.");
+	printf("For full documentation, see man %s\n", PROG_NAME);
 }
--- src/mmv.h
+++ src/mmv.h
@@ -7,19 +7,46 @@
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <getopt.h>
+
+#define PROG_NAME	 "mmv"
+#define PROG_VERSION "version 0.2.0"
+#define PROG_REMOTE	 "https://github.com/mcauley-penney/mmv-c"
 
 typedef u_int32_t Fnv32_t;
 
+struct Opts
+{
+	bool verbose;
+};
+
 struct Set
 {
-    size_t num_keys;
-    char **map;
-    unsigned int keys[];
+	size_t num_keys;
+	char **map;
+	unsigned int keys[];
 };
+
+/**
+ * @brief Create struct of possible user flags
+ * @return Opt struct
+ */
+struct Opts *make_opts(void);
+
+/**
+ * @brief Print program usage information
+ */
+void usage(void);
+
+/**
+ * @brief Print program help information
+ */
+void try_help(void);
 
 /**
  * @brief Create a hashmap of strings from argv
@@ -31,10 +58,24 @@ struct Set
  */
 struct Set *make_str_set(int argc, char *argv[]);
 
+/**
+ * @brief Allocate memory for members of a Set struct
+ *
+ * @param num_args
+ * @param map_size
+ */
 struct Set *alloc_str_set(
-    const unsigned int num_args, const unsigned int map_size);
+	const unsigned int num_args, const unsigned int map_size);
 
-int str_set_insert(char *cur_str, const unsigned int map_size, struct Set *map);
+/**
+ * @brief Insert a string into the given Set struct
+ *
+ * @param cur_str
+ * @param map_size
+ * @param map
+ * @return
+ */
+int str_set_insert(char *cur_str, const unsigned int map_space, struct Set *set);
 
 /**
  * @brief hashes a string with the Fowler–Noll–Vo 1a 32bit hash fn
@@ -81,6 +122,7 @@ FILE *__attribute__((malloc)) open_tmp_path_fptr(char *tmp_path);
  */
 int open_file_in_editor(const char *path);
 
+
 /**
  * @brief reads lines out of a file path and renames item at corresponding
  *      position in hashmap to newly-read line.
@@ -88,20 +130,21 @@ int open_file_in_editor(const char *path);
  *  For example, the first item in the hashmap will be renamed to the first
  *  item read from the file.
  *
- * @param tmp_path: path to open and read lines from
- * @param map: map to store retrieved strings in
- * @param keys: struct containing list of keys of locations of
- *      string pair nodes in hashmap
+ * @param options: struct of user-defined flags
+ * @param set: set of strings to rename
+ * @param path: path to temp file containing new names
+ * @return errno or 0 for success
  */
-int rename_filesystem_items(struct Set *map, char path[]);
+int rename_filesystem_items(struct Opts *options, struct Set *set, char path[]);
 
 /**
  * @brief renames an item in file system
  *
+ * @param options: struct of user-defined flags
  * @param src: old name
  * @param dest: new name
  */
-void rename_path(const char *src, const char *dest);
+void rename_path(struct Opts *options, const char *src, const char *dest);
 
 /**
  * @brief deletes an item at the given path in the file system
